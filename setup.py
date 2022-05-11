4d78772354348f0170a5da2ba46a494014985911
Dani Lerner <dlerner97@gmail.com>
2022-05-11 17:13:59 -0400

    Fixed author email with a @

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='ut_sdk_python',
     version='0.1',
     packages=['utapi'],
-    url='https://github.com/tutorintelligence/ut_sdk_python',
+    url='https://github.com/UmbraTek/ut_sdk_python',
     license='',
     author='',
-    author_email='',
+    author_email='@',
     description=''
 )
